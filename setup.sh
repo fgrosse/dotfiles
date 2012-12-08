b59a545925bf66353c42a183b3e444fc512c2f3c
Friedrich Große <friedrich.grosse@gmail.com>
2012-12-08 12:07:21 +0100

    Added the git-completion shell script to the repo. It can be used later in the bashrc file

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Setting up git"
 rm -f ~/.gitconfig
 ln -s $DIR/gitconfig ~/.gitconfig
+ln -s $DIR/git-completion.sh ~/.git-completion.sh
+echo "Dotfile setup has finished"
 
