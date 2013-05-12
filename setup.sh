db6736ada9f869ce42c1c34176d1d000b2fb3673
Friedrich Große <friedrich.grosse@gmail.com>
2013-05-12 10:24:37 +0200

    Dropped bash support

--- setup.sh
+++ setup.sh
@@ -3,26 +3,11 @@
 echo "Starting dotfiles setup"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Setting up bash.."
-rm -f ~/.bashrc
-ln -s $DIR/bashrc ~/.bashrc
-rm -f ~/.alias.conf
-ln -s $DIR/alias.conf ~/.alias.conf
-rm -f ~/.bash_profile
-ln -s $DIR/bash_profile ~/.bash_profile
-
 echo "Setting up zsh.."
 rm -f ~/.zshenv
 ln -s $DIR/zsh/zshenv.symlink ~/.zshenv
 rm -f ~/.zshrc
 ln -s $DIR/zsh/zshrc.symlink ~/.zshrc
-
-if [ ! -f ~/.bash_local ]; then
-	echo "Created a new file '~/.bash_local'. You can put your system specific configurations (like alternative PATH) in there"
-	cp $DIR/bash_local ~/.bash_local
-else
-	echo "Did not overwrite existing ~/.bash_local"
-fi
 
 # copy xfce4-terminal configuration (if it is installed)
 if [ -d ~/.config/xfce4/terminal/ ]; then
