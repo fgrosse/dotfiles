e1730e1be4de490ef1a393e2911d4c5e00cb98f6
Friedrich Große <friedrich.grosse@gmail.com>
2026-05-03 22:14:09 +0200

    Replace manual Go install and asdf with mise for tool management

--- setup-tools.sh
+++ setup-tools.sh
@@ -8,11 +8,6 @@ set -e -o pipefail
 # This script will install my favorite tools. It is meant to use on
 # primary workstations but not necessarily on a server.
 
-GO_VERSION='1.26.1'
-GO_SHA256_HASH='031f088e5d955bab8657ede27ad4e3bc5b7c1ba281f05f245bcc304f327c987a'
-
-ASDF_VERSION='v0.13.1'
-
 DNF_PACKAGES=(
     'bat'
     'direnv'
@@ -32,10 +27,6 @@ DNF_PACKAGES=(
     'wget'
     'which'
     'zoxide'
-)
-
-GO_TOOLS=(
-    'github.com/fraugster/cwtch@latest'
 )
 
 start_group() {
@@ -63,51 +54,14 @@ if ! command -v zsh &>/dev/null; then
     end_group
 fi
 
-GO_TARGET=/usr/local/go
-install_go() {
-    GO_TAR_FILE="go$GO_VERSION.linux-amd64.tar.gz"
-    GO_DOWNLOAD_URL="https://go.dev/dl/$GO_TAR_FILE"
-
-    start_group ">> Downloading Go $GO_VERSION ..."
-    echo "   URL: $GO_DOWNLOAD_URL"
-    wget "$GO_DOWNLOAD_URL"
-    end_group
-
-    start_group ">> Validating checksum ..."
-    echo "   Expected: $GO_SHA256_HASH"
-    echo "$GO_SHA256_HASH  $GO_TAR_FILE" | sha256sum -c -
-    end_group
-
-    start_group ">> Removing previous Go installation ..."
-    echo "   Path: $GO_TARGET"
-    sudo rm -rf /usr/local/go
-    end_group
-
-    start_group ">> Decompressing archive ..."
-    sudo tar -C "$(dirname $GO_TARGET)" -xzf "$GO_TAR_FILE"
-    end_group
-
-    start_group ">> Cleaning up downloaded files ..."
-    rm "$GO_TAR_FILE"
-    end_group
-
-    start_group ">> Testing Go installation ..."
-    export PATH=/usr/local/go/bin:$PATH
-    go version
-    end_group
-}
-
-if [[ ! -d "$GO_TARGET" ]]; then
-    echo "Installing Go v$GO_VERSION ..."
-    install_go
+start_group "Installing global dev tools using mise"
+if ! command -v mise &>/dev/null; then
+    ./bin/mise install
 fi
 
-# Install tools with Go install
-for tool in "${GO_TOOLS[@]}"; do
-    start_group "Installing $tool ..."
-    go install "$tool"
-    end_group
-done
+mise use --global go
+mise use --global go:github.com/fraugster/cwtch
+end_group
 
 if [[ ! -d ~/.asdf ]]; then
     start_group "Installing asdf (a tool version manager)"
