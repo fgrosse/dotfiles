595bd51cd4b44d080f431e308756693825118ad6
Friedrich Große <friedrich.grosse@gmail.com>
2026-08-30 21:54:03 +0200

    Add nvim (LazyVim) config
    Ignore the Omarchy-managed theme.lua symlink and LazyVim starter
    template boilerplate (LICENSE, README.md).

---
+++ dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,11 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      trigger = {
+        show_on_keyword = false,
+        show_on_trigger_character = false,
+      },
+    },
+  },
+}
